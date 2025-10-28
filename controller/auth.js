71f32da25759912b00687f96977e1b00995953b6
Tobi <obscurityincarnate@gmail.com>
2025-10-28 21:40:47 +0000

    Speculatively implemented sign in and signup requests

--- controller/auth.js
+++ controller/auth.js
@@ -6,44 +6,51 @@ import isSignedIn from "../middleware/is_signed_in.js";
 const router = express.Router();
 
 router.get("/sign-in",isSignedOut, async (req, res)=>{
-    try {
-        res.render("auth/sign-in");
-    } catch (error) {
-        
-    }
+    res.render("auth/sign-in");
 });
-router.post("/sign-in", (req, res)=>{
+router.post("/sign-in",async (req, res)=>{
+    
     try {
+        const username = req.body.username
         console.log(req.body)
+        const user = await users.findOne({username: username});
+        if(!user) res.status(401).send("Invalid Username");
+        if(!bcrypt.compareSync(user.password, req.body.password))res.status(401).send("Invalid Password");
+
+        req.session.user = {
+            _id: user._id,
+            username: user.username
+        }
+
+        req.session.save(()=>{
+            res.redirect("/")
+        })
     } catch (error) {
-        
+        res.status(500).send("Something went wrong. Unable to sign in");
     }
 });
 router.get("/sign-up", isSignedOut, (req, res)=>{
-    try {
-        res.render("auth/sign-up");
-    } catch (error) {
-        
-    }
+    res.render("auth/sign-up");
 });
-router.post("/sign-up", (req, res)=>{
+router.post("/sign-up", async(req, res)=>{
     try {
         const email = req.body.email;
         const password =  req.body.password;
         const username =  req.body.password;
-        if(users.findOne({email: email})) res.status(409).send("Email has already been used");
-        if(users.findOne({username: username})) res.status(409).send("Username is Already Taken");
+        if(await users.findOne({email: email})) res.status(409).send("Email has already been used");
+        if(await users.findOne({username: username})) res.status(409).send("Username is Already Taken");
         if(password !== req.body["confirm-password"]) res.status(409).send("Passwords do not match");
 
-        req.body.password = bcrypt.hash(password, 12);
-        users.create(req.body)
+        req.body.password = await bcrypt.hash(password, 12);
         console.log(req.body)
+        users.create(req.body)
+        console.log("User created Successfully")
     } catch (error) {
-        
+        req.status(400).send("Failed to create the account");
     }
 });
 
 router.post("/sign-out", isSignedIn, (req, res)=>{
-
+    res.redirect("/")
 });
 export default router
